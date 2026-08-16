eadc9b4fcca01a21d9e9de13a4093ec2f951dce3
phi <luongducphi2711@gmail.com>
2026-08-16 18:11:41 +0200

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.py
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -1,22 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # 1. Wenn die Länge ungleich ist, kann es kein Anagramm sein (Frühzeitiger Abbruch)
+
         if len(s) != len(t):
             return False
             
         counts_hashmap = {}
         
-        # splitS und splitT werden hier gar nicht benötigt, da wir direkt über die Strings iterieren
-        
-        # Buchstaben von 's' hochzählen
         for x in s:
             counts_hashmap[x] = counts_hashmap.get(x, 0) + 1
         
-        # Buchstaben von 't' wieder abziehen
         for x in t:
             counts_hashmap[x] = counts_hashmap.get(x, 0) - 1
 
-        # Wenn alle Werte in der HashMap 0 sind, ist es ein Anagramm
         for count in counts_hashmap.values():
             if count != 0:
                 return False
